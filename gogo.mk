980233c14068d8689f1dd9d545ca6cd08dfb159b
tetrate-ci (CircleCI) <tetrate-ci@users.noreply.github.com>
2019-09-26 00:26:42 +0000

    ci: Refresh docker images (#1586)
    This patch refreshes some of the docker images that we used.
    
    Fixes: #1449 #1557 #1585
    
    Signed-off-by: Dhi Aurrahman dio@tetrate.io
    
    Mirrored from https://github.com/tetrateio/tetrate @ 0be5b2c9e125f1207a6fe0903fcf9a4fa21c2e13

--- gogo.mk
+++ gogo.mk
@@ -1,4 +1,26 @@
 # Copyright (c) Tetrate, Inc 2019 All Rights Reserved.
 
-// TODO(dio): Make it pretty.
-MAPS := Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/rpc/status.proto=istio.io/gogo-genproto/googleapis/google/rpc,Mgoogle/rpc/code.proto=istio.io/gogo-genproto/googleapis/google/rpc,Mgoogle/rpc/error_details.proto=istio.io/gogo-genproto/googleapis/google/rpc,Mgoogle/api/annotations.proto=istio.io/gogo-genproto/googleapis/google/api,Mgoogle/api/http.proto=istio.io/gogo-genproto/googleapis/google/api,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,Mopencensus/proto/trace/v1/trace.proto=istio.io/gogo-genproto/opencensus/proto/trace/v1,Mopencensus/proto/trace/v1/trace_config.proto=istio.io/gogo-genproto/opencensus/proto/trace/v1,Mmetrics.proto=istio.io/gogo-genproto/prometheus,Mvalidate/validate.proto=github.com/envoyproxy/protoc-gen-validate/validate
+EMPTY :=
+SPACE := $(EMPTY) $(EMPTY)
+
+IMPORTMAPS := \
+	gogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto, \
+	google/protobuf/any.proto=github.com/gogo/protobuf/types, \
+	google/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor, \
+	google/protobuf/duration.proto=github.com/gogo/protobuf/types, \
+	google/protobuf/struct.proto=github.com/gogo/protobuf/types, \
+	google/protobuf/timestamp.proto=github.com/gogo/protobuf/types, \
+	google/protobuf/wrappers.proto=github.com/gogo/protobuf/types, \
+	google/rpc/status.proto=istio.io/gogo-genproto/googleapis/google/rpc, \
+	google/rpc/code.proto=istio.io/gogo-genproto/googleapis/google/rpc, \
+	google/rpc/error_details.proto=istio.io/gogo-genproto/googleapis/google/rpc, \
+	google/api/annotations.proto=istio.io/gogo-genproto/googleapis/google/api, \
+	google/api/http.proto=istio.io/gogo-genproto/googleapis/google/api, \
+	google/protobuf/empty.proto=github.com/gogo/protobuf/types, \
+	opencensus/proto/trace/v1/trace.proto=istio.io/gogo-genproto/opencensus/proto/trace/v1, \
+	opencensus/proto/trace/v1/trace_config.proto=istio.io/gogo-genproto/opencensus/proto/trace/v1, \
+	metrics.proto=istio.io/gogo-genproto/prometheus, \
+	validate/validate.proto=github.com/envoyproxy/protoc-gen-validate/validate
+
+MAPS_WITH_SPACE := $(foreach MAP,$(IMPORTMAPS),M$(MAP))
+MAPS := $(subst $(SPACE),$(EMPTY),$(MAPS_WITH_SPACE))
