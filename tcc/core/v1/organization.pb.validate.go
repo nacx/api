099bb96a65f793803955f467324ca4d23be1ae0c
tetrate-ci (CircleCI) <tetrate-ci@users.noreply.github.com>
2019-11-01 11:52:53 +0000

    sync tenant response with failed user and team (#2234)
    * sync response with failed user and team
    
    * create team with option to set member
    
    * custom error, enable message name in gogo
    
    * remove invalid member from team
    
    * fix sync logic
    
    * validateTeamMembers return both valid and invalid
    
    Mirrored from https://github.com/tetrateio/tetrate @ e8f8b8438f2b527d9988668cf5efbca2c78ef4e9

--- tcc/core/v1/organization.pb.validate.go
+++ tcc/core/v1/organization.pb.validate.go
@@ -704,6 +704,103 @@ var _ interface {
 	ErrorName() string
 } = SyncTenantRequestValidationError{}
 
+// Validate checks the field values on SyncTenantResponse with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, an error is returned.
+func (m *SyncTenantResponse) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	{
+		tmp := m.GetFailedUsers()
+
+		if v, ok := interface{}(tmp).(interface{ Validate() error }); ok {
+
+			if err := v.Validate(); err != nil {
+				return SyncTenantResponseValidationError{
+					field:  "FailedUsers",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+	}
+
+	{
+		tmp := m.GetFailedTeams()
+
+		if v, ok := interface{}(tmp).(interface{ Validate() error }); ok {
+
+			if err := v.Validate(); err != nil {
+				return SyncTenantResponseValidationError{
+					field:  "FailedTeams",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+// SyncTenantResponseValidationError is the validation error returned by
+// SyncTenantResponse.Validate if the designated constraints aren't met.
+type SyncTenantResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SyncTenantResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SyncTenantResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SyncTenantResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SyncTenantResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SyncTenantResponseValidationError) ErrorName() string {
+	return "SyncTenantResponseValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e SyncTenantResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSyncTenantResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SyncTenantResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SyncTenantResponseValidationError{}
+
 // Validate checks the field values on Team with the rules defined in the proto
 // definition for this message. If any rules are violated, an error is returned.
 func (m *Team) Validate() error {
@@ -1864,3 +1961,71 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = SyncTenantRequest_SyncTeamValidationError{}
+
+// Validate checks the field values on SyncTenantResponse_FailedIds with the
+// rules defined in the proto definition for this message. If any rules are
+// violated, an error is returned.
+func (m *SyncTenantResponse_FailedIds) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	return nil
+}
+
+// SyncTenantResponse_FailedIdsValidationError is the validation error returned
+// by SyncTenantResponse_FailedIds.Validate if the designated constraints
+// aren't met.
+type SyncTenantResponse_FailedIdsValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SyncTenantResponse_FailedIdsValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SyncTenantResponse_FailedIdsValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SyncTenantResponse_FailedIdsValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SyncTenantResponse_FailedIdsValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SyncTenantResponse_FailedIdsValidationError) ErrorName() string {
+	return "SyncTenantResponse_FailedIdsValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e SyncTenantResponse_FailedIdsValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSyncTenantResponse_FailedIds.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SyncTenantResponse_FailedIdsValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SyncTenantResponse_FailedIdsValidationError{}
