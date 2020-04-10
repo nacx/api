2badb01724d569173dd325c66abb05068a5d23b3
Shriram Rajagopalan <rshriram@users.noreply.github.com>
2020-04-10 12:39:12 -0400

    convert internal apis to streaming mode (#23)
    Signed-off-by: Shriram Rajagopalan <rshriram@tetrate.io>

--- tsb/private/v1/private.pb.go
+++ tsb/private/v1/private.pb.go
@@ -295,37 +295,38 @@ func init() {
 func init() { proto.RegisterFile("tsb/private/v1/private.proto", fileDescriptor_ef0f2162cfc880b7) }
 
 var fileDescriptor_ef0f2162cfc880b7 = []byte{
-	// 473 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x54, 0xdd, 0x8a, 0xd3, 0x40,
-	0x14, 0x6e, 0xb6, 0x4b, 0x59, 0x4f, 0xd1, 0x8b, 0x81, 0xad, 0x25, 0x2e, 0xa5, 0x04, 0xc4, 0x8a,
-	0x38, 0xa1, 0x11, 0x6f, 0xbc, 0x10, 0xba, 0xb2, 0x96, 0xbd, 0x71, 0xa5, 0xba, 0x82, 0xde, 0x4d,
-	0xd2, 0x63, 0x8d, 0xa4, 0x99, 0x30, 0x73, 0x12, 0x29, 0xf8, 0x06, 0xbe, 0x83, 0x8f, 0xe2, 0xb5,
-	0x97, 0x3e, 0x82, 0xf4, 0x49, 0x64, 0x26, 0x49, 0x7f, 0xb4, 0xd4, 0xed, 0xdd, 0x99, 0x39, 0xdf,
-	0xf7, 0xe5, 0x9c, 0xef, 0x9c, 0x0c, 0x9c, 0x91, 0x0e, 0xfd, 0x4c, 0xc5, 0x85, 0x20, 0xf4, 0x8b,
-	0x61, 0x1d, 0xf2, 0x4c, 0x49, 0x92, 0xec, 0x8c, 0x90, 0x94, 0x20, 0x8c, 0x25, 0x17, 0x59, 0xcc,
-	0x49, 0x87, 0xbc, 0x06, 0x14, 0x43, 0xb7, 0x63, 0xb8, 0x2a, 0x14, 0x91, 0x21, 0x2a, 0x99, 0x54,
-	0x2c, 0xb7, 0x6b, 0xee, 0x69, 0x91, 0xa1, 0x36, 0x09, 0x1b, 0x54, 0x99, 0x7b, 0x33, 0x29, 0x67,
-	0x09, 0xfa, 0xf6, 0x14, 0xe6, 0x1f, 0x7d, 0x9c, 0x67, 0xb4, 0x28, 0x93, 0x5e, 0x00, 0xc7, 0xd7,
-	0x1a, 0x15, 0x63, 0x70, 0x9c, 0x8a, 0x39, 0x76, 0x9d, 0xbe, 0x33, 0xb8, 0x35, 0xb1, 0x31, 0xeb,
-	0x40, 0x8b, 0x30, 0x15, 0x29, 0x75, 0x8f, 0xec, 0x6d, 0x75, 0xf2, 0xfa, 0x00, 0x6f, 0x6d, 0xf4,
-	0xca, 0xa0, 0x76, 0x30, 0xbd, 0x4b, 0x68, 0x5f, 0x85, 0x9f, 0x31, 0xa2, 0x91, 0x52, 0x62, 0xc1,
-	0x9e, 0x41, 0xab, 0x10, 0x49, 0x8e, 0xda, 0x82, 0xda, 0x81, 0xc7, 0xff, 0x6d, 0xb1, 0xac, 0xb8,
-	0x18, 0xf2, 0x92, 0x38, 0xa9, 0x18, 0xde, 0xb7, 0x23, 0xb8, 0x6d, 0x2a, 0x9c, 0xa0, 0x96, 0xb9,
-	0x8a, 0x50, 0xb3, 0xf7, 0x2b, 0xb5, 0x66, 0xbf, 0x39, 0x68, 0x07, 0x23, 0xbe, 0xcf, 0x30, 0xbe,
-	0x45, 0xe6, 0x75, 0x34, 0x4a, 0xa7, 0xaf, 0x51, 0xcd, 0x63, 0xad, 0x63, 0x99, 0xd6, 0x1f, 0x73,
-	0xbf, 0x3b, 0x70, 0xba, 0x13, 0xc1, 0x9e, 0xc3, 0x89, 0xaa, 0x12, 0x07, 0x34, 0xb1, 0xe2, 0xb0,
-	0x0b, 0x80, 0x6c, 0xa5, 0x66, 0xfd, 0xbc, 0x13, 0xdc, 0xdf, 0xa1, 0x60, 0x26, 0x6b, 0x04, 0x36,
-	0x8a, 0xdb, 0x20, 0x06, 0x3f, 0x9a, 0x70, 0x72, 0x99, 0x12, 0xaa, 0x54, 0x24, 0x2c, 0x85, 0xd3,
-	0x31, 0xd2, 0x28, 0x49, 0xca, 0x69, 0xac, 0x1d, 0x1a, 0xec, 0x77, 0x64, 0x3d, 0x3c, 0xf7, 0xe1,
-	0x7e, 0xe4, 0xc6, 0x10, 0xbd, 0x06, 0x7b, 0x07, 0x9d, 0xeb, 0x6c, 0x2a, 0x08, 0x5f, 0x24, 0xb9,
-	0xa6, 0xcd, 0x91, 0xdc, 0xc0, 0x0b, 0xb7, 0xc3, 0xcb, 0x3d, 0xe4, 0xf5, 0x1e, 0xf2, 0x0b, 0xb3,
-	0x87, 0x5e, 0x83, 0x7d, 0x85, 0xde, 0x18, 0xe9, 0x6f, 0xd1, 0x97, 0x52, 0xbd, 0xc1, 0x04, 0x23,
-	0x92, 0x8a, 0x3d, 0xde, 0xa7, 0x6f, 0x1a, 0xd1, 0x99, 0x88, 0xb0, 0x86, 0x1f, 0xd6, 0x55, 0x02,
-	0x77, 0xc7, 0xb8, 0x36, 0xef, 0xea, 0x4b, 0x8a, 0xd3, 0xf3, 0x85, 0xfd, 0x29, 0xbc, 0xff, 0x6f,
-	0x96, 0xfb, 0xe8, 0x80, 0xed, 0xf3, 0x1a, 0xe7, 0x4f, 0x7f, 0x2e, 0x7b, 0xce, 0xaf, 0x65, 0xcf,
-	0xf9, 0xbd, 0xec, 0x39, 0x1f, 0x1e, 0xcc, 0x62, 0xfa, 0x94, 0x87, 0x3c, 0x92, 0x73, 0x7f, 0x25,
-	0xe3, 0x8b, 0x2c, 0xf6, 0xb7, 0x5f, 0x88, 0xb0, 0x65, 0x4d, 0x7b, 0xf2, 0x27, 0x00, 0x00, 0xff,
-	0xff, 0xb7, 0xf6, 0xb0, 0x88, 0x3a, 0x04, 0x00, 0x00,
+	// 494 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x54, 0xcf, 0x8a, 0x13, 0x4f,
+	0x10, 0xce, 0xec, 0x86, 0xb0, 0xbf, 0x0a, 0xbf, 0x45, 0x1a, 0x8c, 0x61, 0x5c, 0x42, 0x18, 0x10,
+	0x23, 0x62, 0x8f, 0x89, 0x78, 0xf1, 0x20, 0x64, 0x65, 0x0d, 0x7b, 0x71, 0x25, 0xba, 0xf8, 0xe7,
+	0xd6, 0x33, 0x29, 0xe3, 0xb8, 0x93, 0xe9, 0xa1, 0xbb, 0x66, 0x24, 0xe0, 0x1b, 0xf8, 0x0e, 0x9e,
+	0x7d, 0x14, 0x8f, 0x3e, 0x82, 0xe4, 0x49, 0xa4, 0x7b, 0xfe, 0x6c, 0x56, 0x43, 0x34, 0x07, 0x6f,
+	0xd5, 0x5d, 0xf5, 0x7d, 0x5f, 0xd7, 0x57, 0x35, 0x03, 0x47, 0xa4, 0x03, 0x3f, 0x55, 0x51, 0x2e,
+	0x08, 0xfd, 0x7c, 0x58, 0x85, 0x3c, 0x55, 0x92, 0x24, 0x3b, 0x22, 0x24, 0x25, 0x08, 0x23, 0xc9,
+	0x45, 0x1a, 0x71, 0xd2, 0x01, 0xaf, 0x0a, 0xf2, 0xa1, 0xdb, 0x31, 0x58, 0x15, 0x88, 0xd0, 0x00,
+	0x95, 0x8c, 0x4b, 0x94, 0xdb, 0x35, 0xf7, 0xb4, 0x4c, 0x51, 0x9b, 0x84, 0x0d, 0xca, 0xcc, 0xcd,
+	0xb9, 0x94, 0xf3, 0x18, 0x7d, 0x7b, 0x0a, 0xb2, 0x77, 0x3e, 0x2e, 0x52, 0x5a, 0x16, 0x49, 0x6f,
+	0x04, 0xcd, 0x73, 0x8d, 0x8a, 0x31, 0x68, 0x26, 0x62, 0x81, 0x5d, 0xa7, 0xef, 0x0c, 0xfe, 0x9b,
+	0xda, 0x98, 0x75, 0xa0, 0x45, 0x98, 0x88, 0x84, 0xba, 0x7b, 0xf6, 0xb6, 0x3c, 0x79, 0x7d, 0x80,
+	0x97, 0x36, 0x7a, 0x66, 0xaa, 0x36, 0x20, 0xbd, 0x53, 0x68, 0x9f, 0x05, 0x1f, 0x30, 0xa4, 0xb1,
+	0x52, 0x62, 0xc9, 0x1e, 0x41, 0x2b, 0x17, 0x71, 0x86, 0xda, 0x16, 0xb5, 0x47, 0x1e, 0xff, 0xbd,
+	0xc5, 0xe2, 0xc5, 0xf9, 0x90, 0x17, 0xc0, 0x69, 0x89, 0xf0, 0x3e, 0xef, 0xc1, 0xff, 0xe6, 0x85,
+	0x53, 0xd4, 0x32, 0x53, 0x21, 0x6a, 0xf6, 0xa6, 0x66, 0xdb, 0xef, 0xef, 0x0f, 0xda, 0xa3, 0x31,
+	0xdf, 0x66, 0x18, 0xbf, 0x02, 0xe6, 0x55, 0x34, 0x4e, 0x66, 0xcf, 0x51, 0x2d, 0x22, 0xad, 0x23,
+	0x99, 0x54, 0x62, 0xee, 0x17, 0x07, 0xae, 0x6f, 0xac, 0x60, 0x8f, 0xe1, 0x40, 0x95, 0x89, 0x1d,
+	0x9a, 0xa8, 0x31, 0xec, 0x04, 0x20, 0xad, 0xd9, 0xac, 0x9f, 0x87, 0xa3, 0x5b, 0x1b, 0x18, 0xcc,
+	0x64, 0x0d, 0xc1, 0xda, 0xe3, 0xd6, 0x80, 0xa3, 0xaf, 0x4d, 0x38, 0x38, 0x4d, 0x08, 0x55, 0x22,
+	0x62, 0x76, 0x01, 0xd7, 0x26, 0x48, 0xe3, 0x38, 0x7e, 0x25, 0xd5, 0x85, 0x4e, 0x85, 0x31, 0x67,
+	0xb0, 0xdd, 0x8c, 0xcb, 0xb9, 0xb9, 0x77, 0xb6, 0x57, 0xae, 0xcd, 0xcf, 0x6b, 0xdc, 0x77, 0x58,
+	0x04, 0x87, 0x85, 0xd8, 0x93, 0x38, 0xd3, 0x84, 0xea, 0x1f, 0x4a, 0xbd, 0x86, 0xce, 0x79, 0x3a,
+	0x13, 0x84, 0xa5, 0xd4, 0xe5, 0xe8, 0xff, 0xc2, 0x73, 0xb7, 0xc3, 0x8b, 0x7d, 0xe7, 0xd5, 0xbe,
+	0xf3, 0x13, 0xb3, 0xef, 0x5e, 0x63, 0xe0, 0xb0, 0x4f, 0xd0, 0x9b, 0x20, 0xfd, 0x4a, 0xfb, 0x54,
+	0xaa, 0x17, 0x18, 0x63, 0x48, 0x52, 0xb1, 0x7b, 0xdb, 0x14, 0x4c, 0x33, 0xd6, 0xe7, 0xaa, 0x7c,
+	0xa7, 0xce, 0x58, 0x0c, 0x37, 0x26, 0x48, 0xb5, 0xec, 0xd9, 0xc7, 0x04, 0x67, 0xc7, 0x4b, 0xfb,
+	0xf9, 0x79, 0x7f, 0xde, 0x61, 0xf7, 0xee, 0x0e, 0x7b, 0xee, 0x35, 0x8e, 0x1f, 0x7e, 0x5b, 0xf5,
+	0x9c, 0xef, 0xab, 0x9e, 0xf3, 0x63, 0xd5, 0x73, 0xde, 0xde, 0x9e, 0x47, 0xf4, 0x3e, 0x0b, 0x78,
+	0x28, 0x17, 0x7e, 0x4d, 0xe3, 0x8b, 0x34, 0xf2, 0xaf, 0xfe, 0x8b, 0x82, 0x96, 0xb5, 0xed, 0xc1,
+	0xcf, 0x00, 0x00, 0x00, 0xff, 0xff, 0xd8, 0x79, 0x10, 0x53, 0xa4, 0x04, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -341,12 +342,19 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type InternalClient interface {
 	// Used by tsbd. Argument is an object containing the tenant name.
-	// Returns an array of objects everytime there is an update at the
-	// Look at the object metadata of each element to identify the type.
-	GetAllTenantResources(ctx context.Context, in *TenantName, opts ...grpc.CallOption) (*ObjectArray, error)
-	// Used by tsbd to push registry data from a cluster to management plane.
-	// Object is a cluster object containing all the namespaces and services in that cluster.
-	UpdateClusterResources(ctx context.Context, in *v1.Object, opts ...grpc.CallOption) (*types.Empty, error)
+	// Returns an array of objects (including workspaces, settings,
+	// groups, etc. except the cluster data) everytime there is an
+	// update to the model. Look at the object metadata of each element
+	// to identify the type.
+	GetAllWorkspaces(ctx context.Context, in *TenantName, opts ...grpc.CallOption) (Internal_GetAllWorkspacesClient, error)
+	// Used by tsbd. Argument is an object containing the tenant name.
+	// Returns an array of cluster objects everytime there is an update
+	// to one of the clusters not associated with the tenant.
+	GetAllClusters(ctx context.Context, in *TenantName, opts ...grpc.CallOption) (Internal_GetAllClustersClient, error)
+	// Used by tsbd to push registry data from a cluster to management
+	// plane. Updates arrive in a stream. Object is a cluster object
+	// containing all the namespaces and services in that cluster.
+	UpdateClusterResources(ctx context.Context, opts ...grpc.CallOption) (Internal_UpdateClusterResourcesClient, error)
 	// Used by UI.
 	// returns an array of Cluster objects.
 	// Where each cluster has the list of namespaces/services selected by the selector.
@@ -364,22 +372,102 @@ func NewInternalClient(cc *grpc.ClientConn) InternalClient {
 	return &internalClient{cc}
 }
 
-func (c *internalClient) GetAllTenantResources(ctx context.Context, in *TenantName, opts ...grpc.CallOption) (*ObjectArray, error) {
-	out := new(ObjectArray)
-	err := c.cc.Invoke(ctx, "/tetrateio.api.tsb.private.v1.Internal/GetAllTenantResources", in, out, opts...)
+func (c *internalClient) GetAllWorkspaces(ctx context.Context, in *TenantName, opts ...grpc.CallOption) (Internal_GetAllWorkspacesClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_Internal_serviceDesc.Streams[0], "/tetrateio.api.tsb.private.v1.Internal/GetAllWorkspaces", opts...)
 	if err != nil {
 		return nil, err
 	}
-	return out, nil
+	x := &internalGetAllWorkspacesClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
 }
 
-func (c *internalClient) UpdateClusterResources(ctx context.Context, in *v1.Object, opts ...grpc.CallOption) (*types.Empty, error) {
-	out := new(types.Empty)
-	err := c.cc.Invoke(ctx, "/tetrateio.api.tsb.private.v1.Internal/UpdateClusterResources", in, out, opts...)
+type Internal_GetAllWorkspacesClient interface {
+	Recv() (*ObjectArray, error)
+	grpc.ClientStream
+}
+
+type internalGetAllWorkspacesClient struct {
+	grpc.ClientStream
+}
+
+func (x *internalGetAllWorkspacesClient) Recv() (*ObjectArray, error) {
+	m := new(ObjectArray)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *internalClient) GetAllClusters(ctx context.Context, in *TenantName, opts ...grpc.CallOption) (Internal_GetAllClustersClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_Internal_serviceDesc.Streams[1], "/tetrateio.api.tsb.private.v1.Internal/GetAllClusters", opts...)
 	if err != nil {
 		return nil, err
 	}
-	return out, nil
+	x := &internalGetAllClustersClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type Internal_GetAllClustersClient interface {
+	Recv() (*ObjectArray, error)
+	grpc.ClientStream
+}
+
+type internalGetAllClustersClient struct {
+	grpc.ClientStream
+}
+
+func (x *internalGetAllClustersClient) Recv() (*ObjectArray, error) {
+	m := new(ObjectArray)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *internalClient) UpdateClusterResources(ctx context.Context, opts ...grpc.CallOption) (Internal_UpdateClusterResourcesClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_Internal_serviceDesc.Streams[2], "/tetrateio.api.tsb.private.v1.Internal/UpdateClusterResources", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &internalUpdateClusterResourcesClient{stream}
+	return x, nil
+}
+
+type Internal_UpdateClusterResourcesClient interface {
+	Send(*v1.Object) error
+	CloseAndRecv() (*types.Empty, error)
+	grpc.ClientStream
+}
+
+type internalUpdateClusterResourcesClient struct {
+	grpc.ClientStream
+}
+
+func (x *internalUpdateClusterResourcesClient) Send(m *v1.Object) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *internalUpdateClusterResourcesClient) CloseAndRecv() (*types.Empty, error) {
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	m := new(types.Empty)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
 }
 
 func (c *internalClient) GetClusterResourcesForSelector(ctx context.Context, in *v1.NamespaceSelector, opts ...grpc.CallOption) (*ObjectArray, error) {
@@ -403,12 +491,19 @@ func (c *internalClient) GetResourcesOwnedByUser(ctx context.Context, in *User, 
 // InternalServer is the server API for Internal service.
 type InternalServer interface {
 	// Used by tsbd. Argument is an object containing the tenant name.
-	// Returns an array of objects everytime there is an update at the
-	// Look at the object metadata of each element to identify the type.
-	GetAllTenantResources(context.Context, *TenantName) (*ObjectArray, error)
-	// Used by tsbd to push registry data from a cluster to management plane.
-	// Object is a cluster object containing all the namespaces and services in that cluster.
-	UpdateClusterResources(context.Context, *v1.Object) (*types.Empty, error)
+	// Returns an array of objects (including workspaces, settings,
+	// groups, etc. except the cluster data) everytime there is an
+	// update to the model. Look at the object metadata of each element
+	// to identify the type.
+	GetAllWorkspaces(*TenantName, Internal_GetAllWorkspacesServer) error
+	// Used by tsbd. Argument is an object containing the tenant name.
+	// Returns an array of cluster objects everytime there is an update
+	// to one of the clusters not associated with the tenant.
+	GetAllClusters(*TenantName, Internal_GetAllClustersServer) error
+	// Used by tsbd to push registry data from a cluster to management
+	// plane. Updates arrive in a stream. Object is a cluster object
+	// containing all the namespaces and services in that cluster.
+	UpdateClusterResources(Internal_UpdateClusterResourcesServer) error
 	// Used by UI.
 	// returns an array of Cluster objects.
 	// Where each cluster has the list of namespaces/services selected by the selector.
@@ -422,11 +517,14 @@ type InternalServer interface {
 type UnimplementedInternalServer struct {
 }
 
-func (*UnimplementedInternalServer) GetAllTenantResources(ctx context.Context, req *TenantName) (*ObjectArray, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetAllTenantResources not implemented")
+func (*UnimplementedInternalServer) GetAllWorkspaces(req *TenantName, srv Internal_GetAllWorkspacesServer) error {
+	return status.Errorf(codes.Unimplemented, "method GetAllWorkspaces not implemented")
 }
-func (*UnimplementedInternalServer) UpdateClusterResources(ctx context.Context, req *v1.Object) (*types.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateClusterResources not implemented")
+func (*UnimplementedInternalServer) GetAllClusters(req *TenantName, srv Internal_GetAllClustersServer) error {
+	return status.Errorf(codes.Unimplemented, "method GetAllClusters not implemented")
+}
+func (*UnimplementedInternalServer) UpdateClusterResources(srv Internal_UpdateClusterResourcesServer) error {
+	return status.Errorf(codes.Unimplemented, "method UpdateClusterResources not implemented")
 }
 func (*UnimplementedInternalServer) GetClusterResourcesForSelector(ctx context.Context, req *v1.NamespaceSelector) (*ObjectArray, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetClusterResourcesForSelector not implemented")
@@ -439,40 +537,72 @@ func RegisterInternalServer(s *grpc.Server, srv InternalServer) {
 	s.RegisterService(&_Internal_serviceDesc, srv)
 }
 
-func _Internal_GetAllTenantResources_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(TenantName)
-	if err := dec(in); err != nil {
-		return nil, err
+func _Internal_GetAllWorkspaces_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(TenantName)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
 	}
-	if interceptor == nil {
-		return srv.(InternalServer).GetAllTenantResources(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/tetrateio.api.tsb.private.v1.Internal/GetAllTenantResources",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(InternalServer).GetAllTenantResources(ctx, req.(*TenantName))
-	}
-	return interceptor(ctx, in, info, handler)
+	return srv.(InternalServer).GetAllWorkspaces(m, &internalGetAllWorkspacesServer{stream})
 }
 
-func _Internal_UpdateClusterResources_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(v1.Object)
-	if err := dec(in); err != nil {
+type Internal_GetAllWorkspacesServer interface {
+	Send(*ObjectArray) error
+	grpc.ServerStream
+}
+
+type internalGetAllWorkspacesServer struct {
+	grpc.ServerStream
+}
+
+func (x *internalGetAllWorkspacesServer) Send(m *ObjectArray) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func _Internal_GetAllClusters_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(TenantName)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(InternalServer).GetAllClusters(m, &internalGetAllClustersServer{stream})
+}
+
+type Internal_GetAllClustersServer interface {
+	Send(*ObjectArray) error
+	grpc.ServerStream
+}
+
+type internalGetAllClustersServer struct {
+	grpc.ServerStream
+}
+
+func (x *internalGetAllClustersServer) Send(m *ObjectArray) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func _Internal_UpdateClusterResources_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(InternalServer).UpdateClusterResources(&internalUpdateClusterResourcesServer{stream})
+}
+
+type Internal_UpdateClusterResourcesServer interface {
+	SendAndClose(*types.Empty) error
+	Recv() (*v1.Object, error)
+	grpc.ServerStream
+}
+
+type internalUpdateClusterResourcesServer struct {
+	grpc.ServerStream
+}
+
+func (x *internalUpdateClusterResourcesServer) SendAndClose(m *types.Empty) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *internalUpdateClusterResourcesServer) Recv() (*v1.Object, error) {
+	m := new(v1.Object)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
 		return nil, err
 	}
-	if interceptor == nil {
-		return srv.(InternalServer).UpdateClusterResources(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/tetrateio.api.tsb.private.v1.Internal/UpdateClusterResources",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(InternalServer).UpdateClusterResources(ctx, req.(*v1.Object))
-	}
-	return interceptor(ctx, in, info, handler)
+	return m, nil
 }
 
 func _Internal_GetClusterResourcesForSelector_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -516,14 +646,6 @@ var _Internal_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*InternalServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "GetAllTenantResources",
-			Handler:    _Internal_GetAllTenantResources_Handler,
-		},
-		{
-			MethodName: "UpdateClusterResources",
-			Handler:    _Internal_UpdateClusterResources_Handler,
-		},
-		{
 			MethodName: "GetClusterResourcesForSelector",
 			Handler:    _Internal_GetClusterResourcesForSelector_Handler,
 		},
@@ -532,7 +654,23 @@ var _Internal_serviceDesc = grpc.ServiceDesc{
 			Handler:    _Internal_GetResourcesOwnedByUser_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "GetAllWorkspaces",
+			Handler:       _Internal_GetAllWorkspaces_Handler,
+			ServerStreams: true,
+		},
+		{
+			StreamName:    "GetAllClusters",
+			Handler:       _Internal_GetAllClusters_Handler,
+			ServerStreams: true,
+		},
+		{
+			StreamName:    "UpdateClusterResources",
+			Handler:       _Internal_UpdateClusterResources_Handler,
+			ClientStreams: true,
+		},
+	},
 	Metadata: "tsb/private/v1/private.proto",
 }
 
